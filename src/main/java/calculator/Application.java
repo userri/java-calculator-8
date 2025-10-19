09d5b518e5f417a88443f282ea0c99aba6e7468a
임유리 <dladbfl67@naver.com>
2025-10-19 17:10:10 +0900

    refactor : 디버깅용 주석 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -14,7 +14,6 @@ public class Application {
         try {
             // 공백입력시
             if (str.length() <= 0) {
-//                System.out.println("case 1");
                 System.out.println("결과 : 0");
                 return;
             }
@@ -34,27 +33,22 @@ public class Application {
                 // "//"와 "\n" 사이가 오직 한글자이면서 숫자가 아닐 때만 구분자 추가함
                 int a = str.indexOf("//");
                 int b = str.indexOf("\\n");
-//                System.out.println("a: " + a);
-//                System.out.println("b: " + b);
                 // "//"와 "\n" 사이에 한 글자가 있다면 "//"와 "\n" 인덱스 차이는 3
                 if (b - a == 3 && !Character.isDigit(str.charAt(2))) {
                     // 만약 가운데가 숫자가 아니라면 delimeter 리스트에 추가
                     deli.add(str.substring(2, 3));
                     // 커스텀 구분자 이후의 문자열을 따로 저장
                     newStr = str.substring(5);
-                // "//"와 "\n" 사이에 아무글자가 없다면 "//"와 "\n" 인덱스 차이는 2
+                    // "//"와 "\n" 사이에 아무글자가 없다면 "//"와 "\n" 인덱스 차이는 2
                 } else if (b - a == 2) {
                     //
                     newStr = str.substring(4);
                 } else {
-//                    System.out.println("case 2");
                     throw new IllegalArgumentException();
                 }
             } else {
-//                System.out.println("case 3");
                 throw new IllegalArgumentException();
             }
-//            System.out.println("newstr: " + newStr);
             String joinDeli = String.join("", deli);
             try {
                 if (deli.size() < 3) {
@@ -62,7 +56,6 @@ public class Application {
                     while (st.hasMoreTokens()) {
                         int num = Integer.parseInt(st.nextToken());
                         if (num < 0) {
-//                            System.out.println("case 4");
                             throw new IllegalArgumentException();
                         }
                         sum += num;
@@ -72,19 +65,16 @@ public class Application {
                     while (st.hasMoreTokens()) {
                         int num = Integer.parseInt(st.nextToken());
                         if (num < 0) {
-//                            System.out.println("case 5");
                             throw new IllegalArgumentException();
                         }
                         sum += num;
                     }
                 }
             } catch (NumberFormatException e) {
-//                System.out.println("case 6");
                 throw new IllegalArgumentException();
             }
             System.out.println("결과 : " + sum);
         } catch (Exception e) {
-//            System.out.println("case 7");
             throw new IllegalArgumentException();
         }
     }
